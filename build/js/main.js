78ad01d274ed9e6bd1d21ea5fa9b2e33dd2f1d5b
stepan881 <stepanstepanov180@gmail.com>
2020-08-04 03:44:24 +0300

    scrollIntoView, fix head/about-drone

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,26 @@
 const questionsContent = document.querySelector(`.questions__content`);
 const questionsArrows = document.querySelectorAll(`.questions__arrow`);
 const questionsText = document.querySelectorAll(`.questions__item-text`);
+const links = document.querySelectorAll(`a`);
 
 
+
+links.forEach(el => {
+  if (el.hash[0] === `#` && el.href.length > 3) {
+    el.addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+      document.querySelector(el.hash).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
+  } else if (el.href.length = 1) {
+    el.addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+    })
+  }
+  
+})
 
 questionsContent.addEventListener('click', (evt) => {
   evt.preventDefault();
@@ -18,14 +36,13 @@ questionsContent.addEventListener('click', (evt) => {
     });
 
     if (evt.target.closest(`.questions__item`)) {
-      // console.log(evt.target.closest(`.questions__item`).querySelector(`.questions__item-text`));
       evt.target.closest(`.questions__item`).querySelector(`.questions__item-text`).classList.add(`questions__item-text--active`)
     }
   }
+
   document.querySelector(`#questions`).scrollIntoView({
     behavior: 'auto',
     block: 'start'
   })
-
-  
 });
+
